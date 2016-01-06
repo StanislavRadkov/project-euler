b771c7cf86a802696b442a06264cbd17d9aa8a90
StanislavRadkov <Stanislav.vr@gmail.com>
2016-01-06 10:57:57 +0200

    - Problem 5

---
+++ Project-Euler/Problems/Problems/Problem0005.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Utilities;
+
+namespace Problems
+{
+    public class Problem0005 : IProblem<int>
+    {
+        public int Solve()
+        {
+            var number = -1;
+
+            for (number = 1; number < 999999999; number++)
+            {
+                var remainder = 0;
+
+                for (var divider = 20; divider >= 10; divider--)
+                {
+                    remainder = number % divider;
+
+                    if (remainder != 0)
+                        break;
+                }
+
+                if (remainder == 0)
+                    break;
+            }
+
+            return number;
+        }
+    }
+}
