eaf02f4dc186c222c3c225631f87228a090813c2
StanislavRadkov <Stanislav.vr@gmail.com>
2016-01-07 14:31:15 +0200

    - Removed old code;

--- Project-Euler/Problems/Problems/Problem0006.cs
+++ Project-Euler/Problems/Problems/Problem0006.cs
@@ -11,11 +11,6 @@ namespace Problems
             var squareOfSum = 0;
             var sumOfSqaures = 0;
 
-            for (var i = 1; i <= 100; i++)
-            {
-                sumOfSqaures += i * i;
-            }
-
             sumOfSqaures = Enumerable.Range(1, 100).Aggregate((a, b) => a + (b*b));
             squareOfSum = Enumerable.Range(1, 100).Sum();
 
