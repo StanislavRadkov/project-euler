b32172d28cdba09c6ac34c8eeb56fdc6dba74e8e
StanislavRadkov <Stanislav.vr@gmail.com>
2016-03-17 10:32:51 +0200

    Problem 8

--- Project-Euler/Problems/Program.cs
+++ Project-Euler/Problems/Program.cs
@@ -8,7 +8,7 @@ namespace Problems
         {
             var start = DateTime.Now;
             
-            var problem = new Problem0007();
+            var problem = new Problem0008();
             Console.WriteLine(problem.Solve());
 
             var end = DateTime.Now;
