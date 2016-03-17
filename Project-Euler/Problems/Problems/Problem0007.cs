afaec2ea81129de4d59b2e56c38a585750d836ac
StanislavRadkov <Stanislav.vr@gmail.com>
2016-03-17 10:14:32 +0200

    Problem 7

---
+++ Project-Euler/Problems/Problems/Problem0007.cs
@@ -0,0 +1,25 @@
+﻿using Utilities;
+
+namespace Problems
+{
+    public class Problem0007 : IProblem<int>
+    {
+        public int Solve()
+        {
+            var primeCounter = 0;
+            var number = 1;
+
+            while (primeCounter < 10001)
+            {
+                number++;
+
+                if (Utils.IsPrime(number))
+                {
+                    primeCounter++;
+                }
+            }
+
+            return number;
+        }
+    }
+}
