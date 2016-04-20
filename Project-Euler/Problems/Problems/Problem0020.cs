63df95fe5d78dd5cc67a406219352e9a3714e61f
StanislavRadkov <Stanislav.vr@gmail.com>
2016-04-20 10:29:41 +0300

    Problem 20

---
+++ Project-Euler/Problems/Problems/Problem0020.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Numerics;
+using Utilities;
+
+namespace Problems
+{
+    public class Problem0020 : IProblem<int>
+    {
+        public int Solve()
+        {
+            var result = Fact(new BigInteger(100));
+
+            var sum = result.ToString().Select(c => (int) char.GetNumericValue(c)).Sum();
+
+            return sum;
+        }
+
+        public BigInteger Fact(BigInteger n)
+        {
+            return n == 1 ? n : BigInteger.Multiply(n, Fact(BigInteger.Subtract(n, 1)));
+        }
+    }
+}
--- Project-Euler/Problems/Program.cs
+++ Project-Euler/Problems/Program.cs
@@ -8,7 +8,7 @@ namespace Problems
         {
             var start = DateTime.Now;
 
-            var problem = new Problem0019();
+            var problem = new Problem0020();
             Console.WriteLine(problem.Solve());
 
             var end = DateTime.Now;
