e1bae0ce4b83e382da375915a3ea473db5e84dc0
StanislavRadkov <Stanislav.vr@gmail.com>
2016-05-03 12:17:32 +0300

    Problem 22

---
+++ Project-Euler/Problems/Problems/Problem0022.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Linq;
+using Utilities;
+
+namespace Problems
+{
+    public class Problem0022 : IProblem<int>
+    {
+        public int Solve()
+        {
+            var sum = 0;
+
+            using (var file = new StreamReader("..\\..\\Files\\p022_names.txt"))
+            {
+                var names = file.ReadLine()
+                               .Replace("\"", "")
+                               .Split(',')
+                               .OrderBy(x => x)
+                               .ToArray();
+
+                for (var i = 0; i < names.Count(); i++)
+                {
+                    sum += names[i].Sum(c => (int)c - 64) * (i + 1);
+                }
+            }
+
+            return sum;
+        }
+    }
+}
