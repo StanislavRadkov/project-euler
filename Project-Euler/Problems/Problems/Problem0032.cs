354221e78c38000c270207cbee42169eaae7a351
Stanislav Radkov <Stanislav.vr@gmail.com>
2017-04-15 15:38:45 +0300

    Added solution to problem 32

---
+++ Project-Euler/Problems/Problems/Problem0032.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Utilities;
+
+namespace Problems
+{
+    public class Problem0032 : IProblem<int>
+    {
+        public int Solve()
+        {
+            var sum = 0;
+            for (var i = 1; i < 10000; i++)
+            {
+                if (HasPandigitalProduct(i))
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+            
+        public static bool HasPandigitalProduct(int n)
+        {
+            for (var i = 1; i <= n; i++)
+            {
+                if (n % i == 0 && IsPandigital($"{n}{i}{n / i}"))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsPandigital(string s)
+        {
+            return s.Length == 9 && String.Join(String.Empty, s.ToCharArray().OrderBy(x => x)).Equals("123456789");
+        }
+    }
+}
--- Project-Euler/Problems/Program.cs
+++ Project-Euler/Problems/Program.cs
@@ -8,7 +8,7 @@ namespace Problems
         {
             var start = DateTime.Now;
 
-            var problem = new Problem0031();
+            var problem = new Problem0032();
             Console.WriteLine(problem.Solve());
 
             var end = DateTime.Now;
